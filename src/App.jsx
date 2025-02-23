922667ba4f453c177316489254c53d6a5df94486
Ajay Mishra <asap2656@gmail.com>
2025-02-23 22:40:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DashboardCustomer from './Components/DashboardCustomer';
 import TransactionsTable from './Components/TransactionTable';
 import SuspendCustomerAccount from './Components/SuspendCustomerAccount';
 import Filter from './Components/Filter';
-// import OrderDetails from './Components/OrderDetails';
+import OrderDetails from './Components/OrderDetails';
 function App() {
   return (
     <div className='container'>
@@ -16,9 +16,9 @@ function App() {
         <Navbar />
         <Filter />
         <DashboardCustomer />
-        {/* <SuspendCustomerAccount /> */}
-        {/* <TransactionsTable /> */}
-        {/* <OrderDetails /> */}
+        <SuspendCustomerAccount />
+        <TransactionsTable />
+        <OrderDetails />
       </div>
     </div>
   );
